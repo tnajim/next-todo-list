70bb70bb13c0fedbbd2146af78a16cb42f5b3044
Taher Najim <taher.najim@yahoo.com>
2024-10-14 17:49:46 +0800

    disabled caching of notes

--- app/notes/page.tsx
+++ app/notes/page.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 
 async function getNotes() {
-  const res = await fetch('http://127.0.0.1:8090/api/collections/notes/records?page=1&perPage=30');
+  const res = await fetch('http://127.0.0.1:8090/api/collections/notes/records?page=1&perPage=30', { cache: 'no-store' });
   const data = await res.json();
   return data?.items as any[];
 }
