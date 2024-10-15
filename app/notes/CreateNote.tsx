e91e362e6dcf37cc15a87fc0ff9b2340548b3fb1
Taher Najim <taher.najim@yahoo.com>
2024-10-15 17:39:30 +0800

    added useRouter so page doesnt refresh on add note

--- app/notes/CreateNote.tsx
+++ app/notes/CreateNote.tsx
@@ -1,10 +1,14 @@
 "use client";
 
+import { useRouter } from "next/navigation";
 import { useState } from "react";
+
 
 export default function CreateNote() {
   const [title, setTitle] = useState('');
   const [content, setContent] = useState('');
+
+  const router = useRouter();
 
   const create = async () => {
     await fetch('http://127.0.0.1:8090/api/collections/notes/records', {
@@ -20,6 +24,7 @@ export default function CreateNote() {
 
     setContent('');
     setTitle('');
+    router.refresh();
   }
 
   return (
