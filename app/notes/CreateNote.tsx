e1a471d5f2db8a80466461a5132a1161fe5020f8
Taher Najim <taher.najim@yahoo.com>
2024-10-15 17:36:08 +0800

    added create note functionality

---
+++ app/notes/CreateNote.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import { useState } from "react";
+
+export default function CreateNote() {
+  const [title, setTitle] = useState('');
+  const [content, setContent] = useState('');
+
+  const create = async () => {
+    await fetch('http://127.0.0.1:8090/api/collections/notes/records', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        title,
+        content
+      })
+    });
+
+    setContent('');
+    setTitle('');
+  }
+
+  return (
+    <form onSubmit={create}>
+      <h3>Create a New Note</h3>
+      <input type="text" placeholder="Title" value={title} onChange={(e) => setTitle(e.target.value)} />
+      <textarea placeholder="Content" value={content} onChange={(e) => setContent(e.target.value)}></textarea>
+
+      <button type="submit">Create Note</button>
+    </form>
+  )
+}
--- app/notes/page.tsx
+++ app/notes/page.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import styles from './Notes.module.css';
+import CreateNote from "./CreateNote";
 
 async function getNotes() {
   const res = await fetch('http://127.0.0.1:8090/api/collections/notes/records?page=1&perPage=30', { cache: 'no-store' });
@@ -18,6 +19,8 @@ export default async function NotesPage() {
           return <Note key={note.id} note={note} />;
         })}
       </div>
+
+      <CreateNote />
     </div>
   );
 }
